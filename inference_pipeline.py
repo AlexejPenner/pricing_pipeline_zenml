d6a6d5705ad5b5d893d3d7f2d52da95af69d919e
Alexej Penner <thealexejpenner@gmail.com>
2025-10-13 15:10:38 +0200

    Also returning prediction now

--- inference_pipeline.py
+++ inference_pipeline.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Tuple
 import pandas as pd
 
 from zenml import step, pipeline, Model, get_step_context
@@ -7,15 +7,15 @@ from zenml.enums import ModelStages
 
 @step
 def load_new_row(
-    category: str = 'Electronics',
-    discount_offered: bool = True,
-    brand_rating: float = 4.2,
-    num_reviews: int = 150,
-    days_since_release: int = 45,
-    shipping_weight: float = 2.1,
-    competitors_price: float = 199.99,
-    manufacturing_cost: float = 85.50,
-    tax_rate: float = 0.08) -> Annotated[pd.DataFrame, "new_row"]:
+    category: str,
+    discount_offered: bool,
+    brand_rating: float,
+    num_reviews: int,
+    days_since_release: int,
+    shipping_weight: float,
+    competitors_price: float,
+    manufacturing_cost: float,
+    tax_rate: float) -> Annotated[pd.DataFrame, "new_row"]:
     """Load a new row of data."""
     return pd.DataFrame({
         'category': [category],
@@ -30,7 +30,7 @@ def load_new_row(
     })
 
 @step
-def inference(input_data: pd.DataFrame, model_artifact: str = "price_prediction_model") -> Annotated[pd.DataFrame, "inference_predictions"]:
+def inference(input_data: pd.DataFrame, model_artifact: str = "price_prediction_model") -> Tuple[Annotated[pd.DataFrame, "inference_predictions"], Annotated[float, "predicted_price"]]:
     """Load model and run inference on a single synthetic data sample."""
     zenml_model = get_step_context().model
     
@@ -55,7 +55,7 @@ def inference(input_data: pd.DataFrame, model_artifact: str = "price_prediction_
     results_df = X_inference.copy()
     results_df['predicted_price'] = predictions[0]
     
-    return results_df
+    return results_df, predictions[0]
 
 
 @pipeline
@@ -68,10 +68,10 @@ def price_prediction_inference(
     shipping_weight: float = 2.1,
     competitors_price: float = 199.99,
     manufacturing_cost: float = 85.50,
-    tax_rate: float = 0.08
-    ):
+    tax_rate: float = 0.08) -> Annotated[float, "predicted_price"]:
     new_data_point = load_new_row(category, discount_offered, brand_rating, num_reviews, days_since_release, shipping_weight, competitors_price, manufacturing_cost, tax_rate)
-    inference(new_data_point)
+    _, predicted_price = inference(new_data_point)
+    return predicted_price
 
 if __name__ == "__main__":
     price_prediction_inference.with_options(config_path="inference_config.yaml")()
